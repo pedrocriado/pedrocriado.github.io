d88c7dad4afe7a98a7b02a9e5f7be7b4a25db4e4
pedrocriado <pedrocriadof2004@gmail.com>
2024-12-15 13:27:41 -0500

    moved the navigation bar to make chronological sense

--- my_app/views.py
+++ my_app/views.py
@@ -5,7 +5,8 @@ def index(request):
     return render(request, 'index.html')
 def projects(request):
     return render(request, 'projects.html')
-def contact(request):
-    return render(request, 'contact.html')
 def resume(request):
     return render(request, 'resume.html')
+def contact(request):
+    return render(request, 'contact.html')
+
